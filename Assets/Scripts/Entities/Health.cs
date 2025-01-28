35032e1a132b7d23a0ec245b74a8dd9bcb875b4b
Archie Murray <archie.murray@outlook.com>
2025-01-28 09:11:33 +0000

    fix: Fixed naming in Health script (copied from previous projects)

--- Assets/Scripts/Entities/Health.cs
+++ Assets/Scripts/Entities/Health.cs
@@ -9,30 +9,30 @@ using Utilities;
 
 namespace Entity {
     public class Health : MonoBehaviour {
-        public float getPercentHealth => Mathf.Clamp01(currentHealth / maxHealth);
-        public float getCurrentHealth => currentHealth;
-        public float getMaxHealth => maxHealth;
-        public bool isInvulnerable => invulnerable;
-        public Action<float> onDamage;
-        public Action<float> onHeal;
-        public Action onDeath;
-        public Action onInvulnerableDamage;
-        
-        private float invulnerabilityTimer = 0f;
-        private bool invulnerable = false;
-        private Coroutine invulnerabilityReset = null;
+        public float PercentHealth => Mathf.Clamp01(_currentHealth / _maxHealth);
+        public float CurrentHealth => _currentHealth;
+        public float MaxHealth => _maxHealth;
+        public bool Invulnerable => _invulnerable;
+        public Action<float> OnDamage;
+        public Action<float> OnHeal;
+        public Action OnDeath;
+        public Action OnInvulnerableDamage;
 
-        [SerializeField] private float currentHealth;
-        [SerializeField] private float maxHealth;
+        private float _invulnerabilityTimer = 0f;
+        private bool _invulnerable = false;
+        private Coroutine _invulnerabilityReset = null;
+
+        [SerializeField] private float _currentHealth;
+        [SerializeField] private float _maxHealth;
 
         private void Awake() {
-            currentHealth = maxHealth;
+            _currentHealth = _maxHealth;
         }
 
         private void UpdateMaxHealth(float health) {
             Debug.Log($"Updated max health for {name} to {health}");
-            float diff = health - currentHealth;
-            maxHealth = health;
+            float diff = health - _currentHealth;
+            _maxHealth = health;
             Heal(diff);
         }
 
@@ -40,44 +40,44 @@ namespace Entity {
         /// <param name="damage">Damage to apply</param>
         /// <param name="entityPos">Position of entity that applied the knockback</param>
         public void Damage(float damage, Vector2? entityPos = null) {
-            if (currentHealth == 0.0f) { //Don't damage dead things!
+            if (_currentHealth == 0.0f) { //Don't damage dead things!
                 return;
-            } else if (invulnerable) {
-                onInvulnerableDamage?.Invoke();
+            } else if (_invulnerable) {
+                OnInvulnerableDamage?.Invoke();
                 return;
             }
             damage = Mathf.Max(damage, 0.0f);
             if (damage != 0.0f) {
-                currentHealth = Mathf.Max(0.0f, currentHealth - damage);
-                onDamage?.Invoke(damage);
+                _currentHealth = Mathf.Max(0.0f, _currentHealth - damage);
+                OnDamage?.Invoke(damage);
             }
-            if (currentHealth == 0.0f) {
+            if (_currentHealth == 0.0f) {
                 Debug.Log($"{name} is dead");
-                onDeath?.Invoke();
+                OnDeath?.Invoke();
             }
         }
 
         public void Heal(float amount) {
             amount = Mathf.Max(0f, amount);
-            currentHealth = Mathf.Min(maxHealth, currentHealth + amount);
-            onHeal?.Invoke(amount);
+            _currentHealth = Mathf.Min(_maxHealth, _currentHealth + amount);
+            OnHeal?.Invoke(amount);
         }
 
         public void SetInvulnerable(float time) {
-            if (invulnerabilityReset != null) {
-                StopCoroutine(invulnerabilityReset);
+            if (_invulnerabilityReset != null) {
+                StopCoroutine(_invulnerabilityReset);
             }
-            invulnerabilityTimer += time;
-            invulnerabilityReset = StartCoroutine(InvulnerabilityReset());
+            _invulnerabilityTimer += time;
+            _invulnerabilityReset = StartCoroutine(InvulnerabilityReset());
         }
 
         public void SetInvulnerable(bool invulnerable) {
-            this.invulnerable = invulnerable;
+            this._invulnerable = invulnerable;
         }
 
         private IEnumerator InvulnerabilityReset() {
-            while (invulnerabilityTimer >= 0f) {
-                invulnerabilityTimer -= Time.fixedDeltaTime;
+            while (_invulnerabilityTimer >= 0f) {
+                _invulnerabilityTimer -= Time.fixedDeltaTime;
                 yield return Yielders.WaitForFixedUpdate;
             }
         }
